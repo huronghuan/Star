5a805eacae1f7e9871e2e39d84126e06896b9146
huronghuan <huronghuan@hongboict.com>
2018-12-16 12:59:22 +0800

    test complete

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -1,20 +1,32 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import '../../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
 contract StarNotary is ERC721 { 
 
     struct Star { 
         string name; 
+        string starStory;
+        string ra;
+        string dec;
+        string mag;
+        string cen;
     }
 
-    mapping(uint256 => Star) public tokenIdToStarInfo; 
-    mapping(uint256 => uint256) public starsForSale;
+    mapping(uint256 => Star) private tokenIdToStarInfoes; 
+    mapping(uint256 => uint256) private starsForSales;
+    bytes [] internal starCoordination;
 
-    function createStar(string _name, uint256 _tokenId) public { 
-        Star memory newStar = Star(_name);
+    function createStar(string _name,string _story,string _ra,string _dec,string _mag,string _cen, uint256 _tokenId) public { 
+        require(checkIfStarExist(_dec,_mag,_cen) == true);
 
-        tokenIdToStarInfo[_tokenId] = newStar;
+        bytes memory co = abi.encodePacked(bytes(_dec),bytes(_mag),bytes(_cen));
+
+        starCoordination.push(co);
+
+        Star memory newStar = Star(_name,_story,_ra,_dec,_mag,_cen);
+
+        tokenIdToStarInfoes[_tokenId] = newStar;
 
         _mint(msg.sender, _tokenId);
     }
@@ -22,13 +34,13 @@ contract StarNotary is ERC721 {
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
         require(this.ownerOf(_tokenId) == msg.sender);
 
-        starsForSale[_tokenId] = _price;
+        starsForSales[_tokenId] = _price;
     }
 
     function buyStar(uint256 _tokenId) public payable { 
-        require(starsForSale[_tokenId] > 0);
+        require(starsForSales[_tokenId] > 0);
         
-        uint256 starCost = starsForSale[_tokenId];
+        uint256 starCost = starsForSales[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
         require(msg.value >= starCost);
 
@@ -40,5 +52,44 @@ contract StarNotary is ERC721 {
         if(msg.value > starCost) { 
             msg.sender.transfer(msg.value - starCost);
         }
+    }
+    /**
+     * @param  _dec string
+     * @param  _mag string
+     * @param  _cen string
+     * @return Boolean
+     */
+    function checkIfStarExist(string _dec,string _mag,string _cen) public view returns (bool) {
+        bytes memory co = abi.encodePacked(bytes(_dec),bytes(_mag),bytes(_cen));
+
+        for(uint i=0;i<starCoordination.length;i++){
+            bytes memory oco = starCoordination[i];
+            if ( oco.length == co.length && keccak256(oco) == keccak256(co)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param  _tokenId uint256
+     * @return Star
+     */
+    function tokenIdToStarInfo(uint256 _tokenId ) public view returns(string,string,string,string,string,string){
+         Star memory s = tokenIdToStarInfoes[_tokenId];
+         s.ra = string(abi.encodePacked('ra_',bytes(s.ra)));
+         s.dec = string(abi.encodePacked('dec_',bytes(s.dec)));
+         s.mag = string(abi.encodePacked('mag_',bytes(s.mag)));
+         s.cen = string(abi.encodePacked('cen_',bytes(s.cen)));
+
+         return (s.name,s.starStory,s.ra,s.dec,s.mag,s.cen);
+    }
+
+    /**
+     * 获取所有售卖的Star
+     * @return uint256
+     */
+    function starsForSale(uint256 _tokenId) public view returns (uint256){
+        return starsForSales[_tokenId];
     }
 }
--- smart_contracts/test/StarNotaryTest.js
+++ smart_contracts/test/StarNotaryTest.js
@@ -9,9 +9,10 @@ contract('StarNotary', accounts => {
     describe('can create a star', () => { 
         it('can create a star and get its name', async function () { 
             
-            await this.contract.createStar('awesome star!', 1, {from: accounts[0]})
+            await this.contract.createStar('awesome star!','','','','','', 1, {from: accounts[0]})
+            let info = await this.contract.tokenIdToStarInfo(1)
 
-            assert.equal(await this.contract.tokenIdToStarInfo(1), 'awesome star!')
+            assert.equal(info[0], 'awesome star!')
         })
     })
 
@@ -24,10 +25,10 @@ contract('StarNotary', accounts => {
         let starPrice = web3.toWei(.01, "ether")
 
         beforeEach(async function () { 
-            await this.contract.createStar('awesome star!', starId, {from: user1})    
+            await this.contract.createStar('awesome star!','','','','','',  starId, {from: user1})    
         })
 
-        it('user1 can put up their star for sale', async function () { 
+        it('user1 can put up their star for sale and get price by starId', async function () { 
             assert.equal(await this.contract.ownerOf(starId), user1)
             await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
             
@@ -52,6 +53,54 @@ contract('StarNotary', accounts => {
 
                 assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice)
             })
+        })
+    })
+
+    describe('check if star exists',()=>{
+        let user1 = accounts[1]
+        let user2 = accounts[2]
+        let user3 = accounts[3]
+
+        let user1_name ='user1_star'
+        let user1_ra =''
+        let user1_story =''
+        let user1_dec = '1.1'
+        let user1_mag ='1.1'
+        let user1_cen ='1.1'
+        let user2_dec = '1.1'
+        let user2_mag ='1.1'
+        let user2_cen ='1.1'
+        let user3_dec = '1.2'
+        let user3_mag ='1.2'
+        let user3_cen ='1.2'
+
+        let token1 = 1
+
+        beforeEach(async function () { 
+            await this.contract.createStar(user1_name,user1_story,user1_ra,user1_dec,user1_mag,user1_cen, token1, {from: user1})    
+        })
+
+        it('coordinaor equal check',async function(){
+            assert.equal(await this.contract.checkIfStarExist(user2_dec,user2_mag,user2_cen),false)
+        })
+
+        it('coordinaor unequal check',async function(){
+            assert.equal(await this.contract.checkIfStarExist(user3_dec,user3_mag,user3_cen),true)
+        })
+    })
+
+    describe('get star info by tokenId',()=>{
+        it('can get star info', async function () { 
+            
+            await this.contract.createStar('awesome star!','','','','','', 1, {from: accounts[0]})
+            let info = await this.contract.tokenIdToStarInfo(1)
+
+            assert.equal(info[0], 'awesome star!')
+            assert.equal(info[1], '')
+            assert.equal(info[2], 'ra_')
+            assert.equal(info[3], 'dec_')
+            assert.equal(info[4], 'mag_')
+            assert.equal(info[5], 'cen_')
         })
     })
 })
